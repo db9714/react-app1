4935163dac5dd9b8508e600074cecc36f78d643c
Your Name <deepakb@trinesis.com>
2020-07-13 11:27:02 +0530

    Changes for config file

--- src/config.js
+++ src/config.js
@@ -2,16 +2,6 @@ var backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname === 'dev-school.uppercampus.com') {
-  backendHost = 'https://dev-vct.uppercampus.com';
-} else if (hostname === 'qa-school.uppercampus.com') {
-  backendHost = 'https://qa-vct.uppercampus.com';
-} else if (hostname === 'staging-school.uppercampus.com') {
-  backendHost = `https://staging-vct.uppercampus.com`;
-} else if (hostname === 'school.uppercampus.com') {
-  backendHost = `https://admin.uppercampus.com`;
-} else {
-  backendHost = `https://dev-vct.uppercampus.com`;
-}
+// backend host api url based on env
 
 export const API_URL = `${backendHost}/api/`;
